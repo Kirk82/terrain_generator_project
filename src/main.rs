451dda7dfc03bff26aecc7876c280947d77d86c4
Kirk82 <kirkdaly@gmail.com>
2025-10-14 22:13:38 +1100

    assigning colour to noise height value

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,27 @@
-
-
 use image::{flat::SampleLayout, *};
 use opensimplex_noise_rs::*;
 use spatial2d::*;
-use rng::*;
 
 fn main() {
     let mut terrain_map: Matrix<f32> = Matrix::new(UVec2 { x: 1000, y: 1000 });
 
     let noise_generator = OpenSimplexNoise::new(None);
 
-    let rng_generator = Rng::new();
-
-    let rgb = image::Rgb;
-
     for (my_value, my_position) in terrain_map.iter_with_pos_mut() {
         let noise_value = noise_generator.eval_2d(my_position.x.into(), my_position.y.into());
-        let noise_value;
-        my_value = noise_value;
+        *my_value = noise_value as f32;
     }
 
-    fn get_colour() -> image::Rgb<>gb {
-if my_value == rng_generator.gen_range(0.0..1.0), {
-    rgb[0.3, 0, 0.3]}
-};
-    
+    fn get_colour() -> Rgb<f32> {
+        let mut height: f32 = 0.0;
+        if height <= 0.1 {
+            Rgb([0.0, 0.0, 1.0])
+        } else {
+            if height <= 0.3 {
+                Rgb([1.0, 1.0, 0.0])
+            }
+        }
+    }
 
     println!("Hello World");
 }
