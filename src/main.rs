d38ced06f4508047f61eeb1b7937ad80c77f45a7
Kirk82 <kirkdaly@gmail.com>
2025-10-24 20:27:12 +1100

    first time working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::path::{self, Path};
+use std::path::Path;
 
 use image::*;
 use opensimplex_noise_rs::*;
@@ -7,12 +7,13 @@ use spatial2d::*;
 fn main() {
     let mut terrain_map: Matrix<f32> = Matrix::new(UVec2 { x: 1000, y: 1000 });
 
-    let mut image_buffer = Rgb32FImage::new(1000, 1000);
+    let mut image_buffer = RgbImage::new(1000, 1000);
 
     let noise_generator = OpenSimplexNoise::new(None);
 
     for (value, position) in terrain_map.iter_with_pos_mut() {
-        let noise_value = noise_generator.eval_2d(position.x.into(), position.y.into());
+        let noise_value: f64 =
+            noise_generator.eval_2d((position.x as f64) / 40.0, (position.y as f64) / 40.0);
         *value = noise_value as f32;
     }
 
@@ -24,28 +25,28 @@ fn main() {
 
     let path = Path::new("terrain.png");
 
-    let image = DynamicImage::ImageRgb32F(image_buffer);
+    let image = DynamicImage::ImageRgb8(image_buffer);
 
     image
         .save(path)
         .unwrap_or_else(|e| panic!("Failed to save image at {path:#?}: {e}"));
 }
 
-fn get_colour(height: f32) -> Rgb<f32> {
-    if height <= 0.1 {
-        Rgb([0.0, 0.0, 1.0])
+fn get_colour(height: f32) -> Rgb<u8> {
+    if height <= 0.3 {
+        Rgb([0, 0, 255])
     } else {
-        if height <= 0.4 {
-            Rgb([1.0, 1.0, 0.0])
+        if height <= 0.5 {
+            Rgb([255, 255, 0])
         } else {
-            if height <= 0.6 {
-                Rgb([1.0, 1.0, 1.0])
+            if height <= 0.7 {
+                Rgb([0, 255, 0])
             } else {
                 if height <= 0.8 {
-                    Rgb([1.0, 0.0, 0.0])
+                    Rgb([150, 75, 0])
                 } else {
-                    if height <= 1.0 {
-                        Rgb([0.0, 0.0, 0.0])
+                    if height <= 0.9 {
+                        Rgb([255, 255, 255])
                     } else {
                         if height >= 0.9 {}
                         panic!()
