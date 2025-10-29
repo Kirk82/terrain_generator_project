a1dc1925b2683b572a77f6b1070bdcc8ac4a20fd
Liam Hamilton <liam.hamilton1999@hotmail.com>
2025-10-29 23:07:14 +1100

    Code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::path::Path;
-
 use image::*;
 use opensimplex_noise_rs::*;
 use rng::*;
@@ -7,39 +5,31 @@ use spatial2d::*;
 
 fn main() {
     //setting the size of the y and x axis
-    let x_size: u32 = 1000;
+    let map_size = UVec2::new(1000, 1000);
 
-    let y_size = 1000;
-
-    //creating a matrix based on x and y variable sizes
-    let mut terrain_map: Matrix<f32> = Matrix::new(UVec2 {
-        x: x_size,
-        y: y_size,
-    });
+    //creating a matrix based on map_size
+    let mut terrain_map: Matrix<f32> = Matrix::new(map_size);
 
     //creating a an image buffer based on the x and y variable sizes
-    let mut image_buffer = RgbImage::new(x_size, y_size);
+    let mut image_buffer = RgbImage::new(map_size.x, map_size.y);
 
     //creating a variable for the Rng crate
-    let rng_generator = Rng::new();
+    let rng = Rng::new();
 
     //creating a variable to generate a random for the noise generator
-    let random_seed: i64 = rng_generator.gen_value();
+    let random_seed: i64 = rng.gen_value();
 
     //creating a random noise generator variable to be used in the loop
     let noise_generator = OpenSimplexNoise::new(Some(random_seed));
 
-    let matrix_centre: UVec2 = (UVec2 {
-        x: (x_size),
-        y: (y_size),
-    }) / 2;
+    let matrix_centre = map_size / 2;
 
     let max_distance = 500.0 * (2.0_f32.sqrt());
 
     //looping through the matrix and assigning noise values to each value in the matrix
-    for (value, position) in terrain_map.iter_with_pos_mut() {
-        let noise_value: f64 =
-            noise_generator.eval_2d((position.x as f64) / 40.0, (position.y as f64) / 40.0);
+    for (height, position) in terrain_map.iter_with_pos_mut() {
+        let noise_value =
+            noise_generator.eval_2d((position.x as f64) / 40.0, (position.y as f64) / 40.0) as f32;
 
         let distance = matrix_centre.distance_euclidian(position);
 
@@ -47,10 +37,11 @@ fn main() {
 
         let inverted_distance = 1.0 - normalised_distance;
 
-        *value = (((noise_value as f32) + 1.0) / 2.0) * inverted_distance;
+        *height = ((noise_value + 1.0) / 2.0) * inverted_distance;
     }
 
-    //looping through the matrix again and assigning different colours to different values of the noise map and then assigning each pixel to the image buffer
+    //looping through the matrix again and assigning different colours to different values of the noise map
+    //then assigning each pixel to the image buffer
     for (height, position) in terrain_map.iter_with_pos() {
         let pixel = get_colour(*height);
 
@@ -58,7 +49,7 @@ fn main() {
     }
 
     //defining the path the image will be saved to
-    let path = Path::new("terrain.png");
+    let path = "terrain.png";
 
     let image = DynamicImage::ImageRgb8(image_buffer);
 
@@ -75,24 +66,15 @@ fn main() {
 fn get_colour(height: f32) -> Rgb<u8> {
     if height <= 0.1 {
         Rgb([0, 0, 255])
+    } else if height <= 0.15 {
+        Rgb([255, 255, 0])
+    } else if height <= 0.5 {
+        Rgb([0, 255, 0])
+    } else if height <= 0.7 {
+        Rgb([150, 75, 0])
+    } else if height <= 1.0 {
+        Rgb([255, 255, 255])
     } else {
-        if height <= 0.5 {
-            Rgb([255, 255, 0])
-        } else {
-            if height <= 0.7 {
-                Rgb([0, 255, 0])
-            } else {
-                if height <= 0.8 {
-                    Rgb([150, 75, 0])
-                } else {
-                    if height <= 1.0 {
-                        Rgb([255, 255, 255])
-                    } else {
-                        if height >= 1.1 {}
-                        panic!()
-                    }
-                }
-            }
-        }
+        panic!("Height should not be greater than 1.0")
     }
 }
