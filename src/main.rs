10e422f8fbfd1dee935910cb71186fb9b1956251
Kirk82 <kirkdaly@gmail.com>
2025-11-03 20:14:40 +1100

    layered noise function working

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,16 @@ use rng::*;
 use spatial2d::*;
 
 fn main() {
+    let size = 1000;
+
     //setting the size of the y and x axis
-    let size = UVec2::new(1000, 1000);
-
-    let rng = Rng::new();
-
-    let extra_distance = rng.gen_bool(0.5);
+    let grid_size = UVec2::splat(size);
 
     //creating a matrix based on x and y variable sizes
-    let mut terrain_map: Matrix<f32> = Matrix::new(UVec2 {
-        x: size.x,
-        y: size.y,
-    });
+    let mut terrain_map: Matrix<f32> = Matrix::new(grid_size);
 
     //creating a an image buffer based on the x and y variable sizes
-    let mut image_buffer = RgbImage::new(size.x, size.y);
+    let mut image_buffer = RgbImage::new(grid_size.x, grid_size.y);
 
     //creating a variable for the Rng crate
     let rng_generator = Rng::new();
@@ -29,57 +24,41 @@ fn main() {
     //creating a random noise generator variable to be used in the loop
     let noise_generator = OpenSimplexNoise::new(Some(random_seed));
 
-    let matrix_centre: UVec2 = (UVec2 {
-        x: (size.x),
-        y: (size.y),
-    }) / 2;
+    let matrix_centre: UVec2 = grid_size / 2;
 
-    let max_distance_x = size.x / 2;
-
-    let max_distance_y = size.y / 2;
-
-    if extra_distance == true {
-        max_distance_x * 2;
-    } else if extra_distance == false {
-        max_distance_y * 2;
-    }
-
-    let max_distance = UVec2::new(max_distance_x, max_distance_y);
+    let max_distance = size as f32 / 2.0;
 
     //looping through the matrix and assigning noise values to each value in the matrix
-    for (value, position) in terrain_map.iter_with_pos_mut() {
-        let first_noise_value: f64 =
-            noise_generator.eval_2d((position.x as f64) / 10.0, (position.y as f64) / 10.0);
-
-        let second_noise_value =
-            noise_generator.eval_2d((position.x as f64) / 30.0, (position.y as f64) / 30.0);
-
-        let third_noise_value =
-            noise_generator.eval_2d((position.x as f64) / 10.0, (position.y as f64) / 10.0);
-
+    for (height, position) in terrain_map.iter_with_pos_mut() {
         let distance = matrix_centre.distance_euclidian(position);
+        let normalised_distance = (distance / max_distance).min(1.0);
 
-        let normalised_x_distance = (distance / max_distance_x as f32).min(1.0);
+        // assert!(normalised_distance >= 0.0 && normalised_distance <= 1.0);
 
-        let normalised_y_distance = (distance / max_distance_y as f32).min(1.0);
+        let inverted_distance = 1.0 - normalised_distance;
 
-        let inverted_distance;
+        let noise_value = layered_noise(&noise_generator, position, 160.0, 8);
 
-        if extra_distance == true {
-            let inverted_distance = 1.0 - normalised_x_distance;
-        } else if extra_distance == false {
-            let inverter_distance = 1.0 - normalised_y_distance;
-        }
+        *height = noise_value * inverted_distance;
 
-        let first_value = (((first_noise_value as f32) + 1.0) / 2.0) * inverted_distance;
-        let second_value = (((second_noise_value as f32) + 1.0) / 2.0) * inverted_distance;
-        let third_value = (((third_noise_value as f32) + 1.0) / 2.0) * inverted_distance;
+        // assert!(!height.is_nan());
 
-        *value = first_value * second_value * third_value;
+        // assert!(
+        //     *height >= 0.0 && *height <= 1.0,
+        //     "height: {}, pos: {}, inv_dist: {}",
+        //     *height,
+        //     position,
+        //     inverted_distance
+        // );
 
-        let square_root = value.sqrt();
+        // if position == UVec2::new(0, 0) {
+        //     dbg!(inverted_distance, square_root, &height);
+        // }
 
-        *value = lerp(*value, square_root, inverted_distance);
+        // if position == UVec2::new(500, 500) {
+        //     dbg!(inverted_distance, square_root, &height);
+        //     panic!()
+        // }
     }
 
     //looping through the matrix again and assigning different colours to different values of the noise map and then assigning each pixel to the image buffer
@@ -105,34 +84,44 @@ fn main() {
 }
 
 fn get_colour(height: f32) -> Rgb<u8> {
-    if height <= 0.1 {
-        Rgb([0, 0, 255])
+    if height <= 0.2 {
+        Rgb([0, 0, 255]) // Water
+    } else if height <= 0.22 {
+        Rgb([190, 190, 0]) // Sand
+    } else if height <= 0.3 {
+        Rgb([230, 230, 0]) // 
+    } else if height <= 0.5 {
+        Rgb([0, 160, 0]) // 
+    } else if height <= 0.8 {
+        Rgb([150, 75, 0]) // 
+    } else if height <= 0.9 {
+        Rgb([255, 255, 255]) // 
     } else {
-        if height <= 0.2 {
-            Rgb([190, 190, 0])
-        } else {
-            if height <= 0.4 {
-                Rgb([230, 230, 0])
-            } else {
-                if height <= 0.5 {
-                    Rgb([0, 160, 0])
-                } else {
-                    if height <= 0.8 {
-                        Rgb([150, 75, 0])
-                    } else {
-                        if height <= 1.0 {
-                            Rgb([255, 255, 255])
-                        } else {
-                            if height >= 1.0 {}
-                            panic!()
-                        }
-                    }
-                }
-            }
-        }
+        panic!()
     }
 }
 
-fn lerp(from: f32, to: f32, s: f32) -> f32 {
-    from + (to - from) * s
+fn layered_noise(
+    noise_generator: &OpenSimplexNoise,
+    pos: UVec2,
+    scale: f32,
+    layer_count: u32,
+) -> f32 {
+    let mut total_noise_value = 0.0;
+    let mut amp = 1.0;
+    let mut noise_scale = scale;
+    let mut total_amp = 0.0;
+
+    for _ in 0..layer_count {
+        let scaled_pos = pos.as_vec2() / noise_scale;
+        total_noise_value +=
+            noise_generator.eval_2d(scaled_pos.x as f64, scaled_pos.y as f64) as f32 * amp;
+
+        total_amp += amp;
+        amp /= 2.0;
+        noise_scale /= 2.0;
+    }
+
+    // ((total_noise_value + 1.0) / 2.0) / total_amp
+    ((total_noise_value / total_amp) + 1.0) / 2.0
 }
